56b7d4cd24cbfc45bcaaec75e1659e4258ed0b6c
feng <oscarfli@163.com>
2024-11-23 21:40:26 +0100

    change nothing

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 print(DB_USER)
-
+print(DB_PASSWORD)
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
